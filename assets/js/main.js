4e82f0cb486fb0f3ae910db0238809c5245cb102
michellejames <shellyjames90@yahoo.com>
2017-12-05 00:22:53 -0500

    Updated a lot

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
 //console.log("Hello World from main.js!");
+
+
+//Infowindows swipping in from sides
+$(".explore").waypoint(function () {
+	var myVideo = document.getElementById("explore__video");
+	myVideo.play();
+}, {
+	offset: '40%',
+	triggerOnce: true,
+})
